af40b9542dc01b63cb4b4aba6a0343d5e228200a
Dante Tucker <dante@dantetucker.com>
2025-04-30 09:44:45 -0400

    Real Initial commit

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using ResoniteModLoader;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamRecentUsers
+{
+    public class SteamRecentUsers : ResoniteMod
+    {
+        public override string Name => "SteamRecentUsers";
+        public override string Author => "Dante";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/YourNameHere/SteamRecentUsers"; 
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("es.dant.SteamRecentUsers");
+            harmony.PatchAll();
+        }
+
+        public static async void OnUserJoined(User user)
+        {
+            if (user.IsLocalUser)
+                return;
+
+            await AddUser(user);
+
+        }
+
+        public static async Task AddUser(User user)
+        {
+            while (!user.IsPresent)
+            {
+                await Task.Delay(1000);
+            }
+
+            if (user.Metadata.TryGetElement("SteamID", out var steamIdData))
+            {
+                if (steamIdData.TryGetValue<ulong>(out var steamId))
+                {
+                    SteamFriends.SetPlayedWith(new CSteamID(steamId));
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(World), "StartRunning")]
+        class World_StartRunning_Patch
+        {
+            static void Postfix(World __instance)
+            {
+                __instance.UserJoined += OnUserJoined;
+            }
+        }
+    }
+}
