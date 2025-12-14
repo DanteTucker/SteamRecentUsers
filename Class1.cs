4ea21b438d276a5b556f37eaa5db2254e572e537
Dante Tucker <dante@dantetucker.com>
2025-12-14 10:26:29 -0500

    Bump version

--- Class1.cs
+++ Class1.cs
@@ -14,7 +14,7 @@ namespace SteamRecentUsers
     {
         public override string Name => "SteamRecentUsers";
         public override string Author => "Dante";
-        public override string Version => "1.0.1";
+        public override string Version => "1.0.2";
         public override string Link => "https://github.com/DanteTucker/SteamRecentUsers"; 
 
         public override void OnEngineInit()
